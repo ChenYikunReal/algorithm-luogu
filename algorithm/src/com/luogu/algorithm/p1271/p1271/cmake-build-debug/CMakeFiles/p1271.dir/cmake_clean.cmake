21becbf584735c7471199905b8a8892c45d4e5ed
ChenYikunReal <1916344726@qq.com>
2020-03-14 01:22:32 +0800

    2020/3/13补交

---
+++ algorithm/src/com/luogu/algorithm/p1271/p1271/cmake-build-debug/CMakeFiles/p1271.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p1271.dir/main.cpp.obj"
+  "libp1271.dll.a"
+  "p1271.exe"
+  "p1271.exe.manifest"
+  "p1271.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p1271.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
