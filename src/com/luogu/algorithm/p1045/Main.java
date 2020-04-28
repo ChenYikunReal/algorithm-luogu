4426cc5671cbeb178d0de6f6a8e60480a39a48b0
ChenYikunReal <50993163+ChenYikunReal@users.noreply.github.com>
2020-04-28 11:39:53 +0800

    2020/4/28日常提交

--- src/com/luogu/algorithm/p1045/Main.java
+++ src/com/luogu/algorithm/p1045/Main.java
@@ -2,32 +2,33 @@ package com.luogu.algorithm.p1045;
 
 import static java.math.BigInteger.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
-import java.util.Scanner;
 
-/**
- * 目前尚不能AC
- * 需要真正设计算法而不是直接用BigInteger算出来
- */
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        BigInteger num = TWO.pow(Integer.parseInt(scanner.nextLine())).subtract(ONE);
-        String string = num.toString();
-        int length = string.length();
-        System.out.println(length);
-        if (length <= 500) {
-            StringBuilder builder = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String p = reader.readLine();
+        BigInteger num = new BigInteger("2").modPow(new BigInteger(p), new BigInteger("10").pow(500)).subtract(ONE);
+        reader.close();
+        int length = num.toString().length();
+        StringBuilder result = new StringBuilder();
+        if (length < 500) {
             for (int i = 0; i < 500-length; i++) {
-                builder.append("0");
+                result.append('0');
             }
-            string = builder.append(string).toString();
+            result.append(num);
         } else {
-            string = string.substring(length-500);
+            result.append(num);
         }
-        scanner.close();
+        String str = result.toString();
+        result = new StringBuilder();
+        result.append((int)(Math.log10(2)*Integer.parseInt(p))+1).append('\n');
         for (int i = 0; i < 10; i++) {
-            System.out.println(string.substring(i*50, i*50+50));
+            result.append(str, i*50, i*50+50).append('\n');
         }
+        System.out.println(result);
     }
 }
