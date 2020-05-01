0a6a4a40e0230c63aed24c08223ffaf4665cd786
ChenYikunReal <50993163+ChenYikunReal@users.noreply.github.com>
2020-05-01 23:56:35 +0800

    2020.5.1日常提交

---
+++ out/production/algorithm/com/luogu/algorithm/p1923/p1923/cmake-build-debug/CMakeFiles/p1923.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p1923.dir/main.cpp.obj"
+  "libp1923.dll.a"
+  "p1923.exe"
+  "p1923.exe.manifest"
+  "p1923.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p1923.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
