1a1981d5d8cc7ebd109019e557722f666fb6e33d
ChenYikunReal <50993163+ChenYikunReal@users.noreply.github.com>
2020-05-03 14:47:19 +0800

    2020.5.3日常提交

--- src/com/luogu/algorithm/p1981/Main.java
+++ src/com/luogu/algorithm/p1981/Main.java
@@ -4,6 +4,9 @@ import java.math.BigInteger;
 import java.util.Scanner;
 import java.util.Stack;
 
+/**
+ * WA
+ */
 public class Main {
     public static void main(String[] args) {
         Stack<BigInteger> stack = new Stack<>();
@@ -15,20 +18,37 @@ public class Main {
             System.out.println(new BigInteger(expression).mod(MOD_NUM));
             return;
         }
+        boolean isAdd = false, isMultiply = false;
         StringBuilder temp_num = new StringBuilder();
         for (char c : expression.toCharArray()) {
             if (c == '+') {
-                if (stack.size() == 2) {
-                    stack.push(stack.pop().add(stack.pop()));
+                if (isAdd) {
+
+                }
+                if (stack.size() >= 2) {
+                    if (isAdd) {
+                        stack.push(stack.pop().add(stack.pop()));
+                        stack.push(new BigInteger(temp_num.toString()));
+                        temp_num = new StringBuilder();
+                    }
+                    if (isMultiply) {
+                        stack.push(stack.pop().multiply(new BigInteger(temp_num.toString())));
+                        temp_num = new StringBuilder();
+                    }
+                } else {
+                    isAdd = true;
                     stack.push(new BigInteger(temp_num.toString()));
+                    temp_num = new StringBuilder();
+                }
+            } else if (c == '*') {
+                if (isMultiply) {
+                    stack.push(stack.pop().multiply(new BigInteger(temp_num.toString())));
                     temp_num = new StringBuilder();
                 } else {
                     stack.push(new BigInteger(temp_num.toString()));
                     temp_num = new StringBuilder();
                 }
-            } else if (c == '*') {
-                stack.push(stack.pop().multiply(new BigInteger(temp_num.toString())));
-                temp_num = new StringBuilder();
+                isMultiply = true;
             } else {
                 temp_num.append(c);
             }
