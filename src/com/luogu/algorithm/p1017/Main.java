81e40e111eb1c0fcff6b750018a1647210e13aa9
ChenYikunReal <50993163+ChenYikunReal@users.noreply.github.com>
2020-05-08 17:40:58 +0800

    2020.5.8日常提交

--- src/com/luogu/algorithm/p1017/Main.java
+++ src/com/luogu/algorithm/p1017/Main.java
@@ -2,18 +2,37 @@ package com.luogu.algorithm.p1017;
 
 import java.util.Scanner;
 
-/**
- * WA的
- */
 public class Main {
+
+    private static StringBuilder result = new StringBuilder();
+
+    private static void toString(int num, int radix) {
+        if (num==0) {
+            return;
+        }
+        int remainder = num % radix;
+        if (remainder < 0) {
+            remainder -= radix;
+            num += radix;
+        }
+        if (remainder >= 10) {
+            remainder += 'A'-10;
+        } else {
+            remainder += '0';
+        }
+        toString(num/radix, radix);
+        result.append((char)remainder);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int origin_num = -scanner.nextInt();
-        int radix = -scanner.nextInt();
+        int origin_num = scanner.nextInt();
+        int radix = scanner.nextInt();
         scanner.close();
-        StringBuilder result = new StringBuilder();
-        result.append(origin_num).append("=").append(Integer.toString(origin_num, radix));
-        result = new StringBuilder(result.toString().toUpperCase()).append("(base").append(radix).append(")");
+        result.append(origin_num).append("=");
+        toString(origin_num, radix);
+        result.append("(base").append(radix).append(")");
         System.out.println(result);
     }
+
 }
