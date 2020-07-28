d95146c656806d5f00c6731e1c7c6ae4ff4b26de
ChenYikunReal <50993163+ChenYikunReal@users.noreply.github.com>
2020-07-28 18:30:27 +0800

    清空重来

--- src/com/luogu/algorithm/p2393/p2393/cmake-build-debug/CMakeFiles/p2393.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for p2393.
-# This may be replaced when dependencies are built.
--- src/com/luogu/algorithm/p2666/Pow.java
+++
@@ -1,9 +0,0 @@
-package com.luogu.algorithm.p2666;
-
-public class Pow {
-    public static void main(String[] args) {
-        for (int i = 0; i <= 100; i++) {
-            System.out.print((i*i) + ",");
-        }
-    }
-}
